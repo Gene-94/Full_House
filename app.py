251ba23011ed9c457b5d24d0a43f303a698f2119
Gene-94 <yevheniykushnirenko@gmail.com>
2022-07-28 23:56:59 +0100

    experimenting with flask_restplus

--- app.py
+++ app.py
@@ -1,25 +1,43 @@
 import mysql.connector
 from mysql.connector import Error
+from flask import Flask
+from flask_restplus import Api, Resource
+
+flask_app = Flask(__name__)
+app = Api(app = flask_app)
+
+name_space = app.namespace('api', description='Full House Api')
+
+@name_space.route('/')
+class MainClass(Resource):
+    def get(self):
+        return {
+            "status" : "Got new data"
+        }
+    def post(self):
+        return {
+            "status" : "Posted new data"
+        }
 
 
-try:
-    db = mysql.connector.connect(host='localhost',
-                                    database='Shows',
-                                    user='fullhouse',
-                                    password='my_flask_API')
-    if db.is_connected():
-        print('Connected successfully to database')
-except Error as e:
-    print(e)
+# try:
+#     db = mysql.connector.connect(host='localhost',
+#                                     database='Shows',
+#                                     user='fullhouse',
+#                                     password='my_flask_API')
+#     if db.is_connected():
+#         print('Connected successfully to database')
+# except Error as e:
+#     print(e)
 
-cur = db.cursor()
-query = 'select name, id from artists'
-cur.execute(query)
-for (name, id) in cur:
-    print("artist name: {}\nartist id: {}").format(name, id)
+# cur = db.cursor()
+# query = 'select name, id from artists'
+# cur.execute(query)
+# for (name, id) in cur:
+#     print("artist name: {}\nartist id: {}").format(name, id)
 
-if db is not None and db.is_connected():
-    db.close()    
+# if db is not None and db.is_connected():
+#     db.close()    
 #connect to existing db
 
     
